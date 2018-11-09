9816b16894621aac87a1a3e4bdf0cd917f98ce34
alireza rahi <alireza.rahi@outlook.com>
2018-11-10 00:00:55 +0330

    first commit

---
+++ src/org/studyeasy/person/NonVegan.java
@@ -0,0 +1,12 @@
+package org.studyeasy.person;
+
+public class NonVegan extends Person {
+
+	@Override
+	public void eat() {
+		System.out.println("Eats non vegan food");
+		
+	}
+	
+
+}
---
+++ src/org/studyeasy/person/Person.java
@@ -0,0 +1,20 @@
+package org.studyeasy.person;
+
+public abstract class Person implements IsAlive,LiveLife {
+	public void speak() {
+		System.out.println("Share his/her thoughts.");
+	}
+	@Override
+	public void breathe() {
+		System.out.println("be alive;remain living");
+		
+	}
+	@Override
+	public void message() {
+		System.out.println("life is a journey, keep moving and meet new people");
+	}
+
+	
+	public abstract void eat();
+
+}
